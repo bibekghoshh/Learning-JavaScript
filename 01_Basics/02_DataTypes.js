6820d4e5151d57b8093e6b38c193ddfbfbb98876
Bibek Ghosh <58251209+bibekghoshh@users.noreply.github.com>
2023-08-08 08:54:04 +0000

    Datatypes done

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,9 @@
+let name="Bibek Ghosh"
+let age=23
+var liveInVillage=true
+let state=null
+let future;
+
+console.table([name,age,liveInVillage,state,future]);
+
+console.table([typeof name,typeof age, typeof liveInVillage, typeof state, typeof future]);
