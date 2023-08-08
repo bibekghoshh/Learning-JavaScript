ae579c7672589ed3265704719e7d4c85b4105e64
Bibek Ghosh <58251209+bibekghoshh@users.noreply.github.com>
2023-08-08 06:51:03 +0000

    variables

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,11 @@
+const Id=4356;
+let email="bibek@google.com";
+var password="bb2343";
+city="kolkata";
+
+// Id=45643;    //not allowed to modify const variable in Js
+
+city="hydrabad";
+let state;
+
+console.table([Id,email,password,city,state]);
