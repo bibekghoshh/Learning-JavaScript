c29a0e1439b2811cf9e6e858be1dee10615f9d0f
Bibek Ghosh <58251209+bibekghoshh@users.noreply.github.com>
2023-08-09 12:35:25 +0000

    nums ans Maths done

---
+++ 01_Basics/05_nums_and_Math.js
@@ -0,0 +1,19 @@
+const bal=32;
+const balence= new Number(3423);
+// console.table([bal, typeof bal, balence, typeof balence]);
+// console.log(balence);
+
+// console.log(balence.toString());
+
+const rs=100000000;
+// console.log(rs.toLocaleString("en-IN"));
+
+console.log(Math.abs(-32));
+console.log(Math.round(5.7));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(7.8));
+console.log(Math.min(5,6,9,1,2,4));
+console.log(Math.max(5,6,7,9,12,4,3,2,1));
+
+
+console.log(Math.floor(Math.random()*100));
