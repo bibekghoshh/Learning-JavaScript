846c8345ed98c668f87a73e4360dae3006472738
Bibek Ghosh <58251209+bibekghoshh@users.noreply.github.com>
2023-08-11 16:56:41 +0000

    basis obeject

---
+++ 02_Basics/02_Objects.js
@@ -0,0 +1,11 @@
+const jsUser={
+    name:"Bibek",
+    age:23,
+    location:"amarkanan",
+    email:"bibek@google.com",
+    ["mySym"]:"myKey1",
+}
+console.log(jsUser.email);
+console.log(jsUser["name"]);
+
+const mySym=Symbol("key1");
